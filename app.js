b684ac4c38b6e603be7a53cc004aefd900d08957
Matiasstewart <matiasstewartusher@hotmail.com>
2021-08-29 21:19:48 -0300

    primer desafio levantamos servidor exitosamente

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get (
+    '/',
+    (req, res)=>{
+    res.send ("Hola mundo")
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto http://localhost:3000/ !');
+});
+
