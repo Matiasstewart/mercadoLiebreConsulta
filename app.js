b3b6e9ffa709851de4e695dba4de9edeb30b86c3
Matiasstewart <matiasstewartusher@hotmail.com>
2021-08-29 22:47:56 -0300

    hacemos funcionar el servidor correctamente

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get (
-    '/',
-    (req, res)=>{
-    res.send ("Hola mundo")
+app.use(express.static('public'));
+
+app.get ('/',(req, res)=>{
+    res.sendFile (path.resolve (__dirname ,'views/home.html'))
 });
 
 app.listen(3000, () => {
